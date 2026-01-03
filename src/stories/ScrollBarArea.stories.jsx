47ab90ae56c4c19cf624f6d372268583f23a5634
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-03 18:18:16 +0000

    scrollbar story, importing the scrollbar component

---
+++ src/stories/ScrollBarArea.stories.jsx
@@ -0,0 +1,8 @@
+import ScrollBarArea from "../components/Custom Scroll Bar/ScrollBarArea"
+
+export default {
+    title: "Scrollbar Area",
+    component: ScrollBarArea,
+}
+
+export const Component1 =()=><ScrollBarArea></ScrollBarArea>
