2f0d86019aa4d8f30e0eebcbf5b64cfb4064cc86
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-03 18:11:38 +0000

    Currently the scrollbar is faking a border with position absolute

---
+++ src/components/Custom Scroll Bar/ScrollBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ScrollBar=()=>{
+
+    return(
+        <div className="scrollbar">
+            
+
+            <div className="fake-border">
+
+                <div className="border"></div>
+            </div>
+        </div>
+    )
+}
+
+export default ScrollBar
