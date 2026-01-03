785447465c39884d928a5af95f02101392578c52
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-03 20:58:40 +0000

    logging position and chamging position

--- src/components/Custom Scroll Bar/ScrollBar.jsx
+++ src/components/Custom Scroll Bar/ScrollBar.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+
 
 const ScrollBar=()=>{
+    
+    const scrollbarRef = useRef(null);
+    const [position, setPosition] = useState(0);
+    const [resize, setResize] = useState(false);
+
+    const handleMouseDown = (click)=>{
+        const element = click.target
+        const computedStyle = window.getComputedStyle(element)
+        setResize(true)
+    }
+
+    const handleMouseUp = ()=>{
+        setResize(false)
+    }
+
+    const handleMouseMove = (mousePosition)=>{
+        const rect = scrollbarRef.current.getBoundingClientRect();
+        const borderPosition = mousePosition.clientX - rect.right;
+        if(borderPosition<=0 && borderPosition >= rect.width*-1) setPosition(-borderPosition)
+         console.log("x relative to scrollbar:", borderPosition);
+    }
+
+    useEffect(()=>{
+        if (!resize) return;
+        window.addEventListener('mousemove', handleMouseMove)
+        window.addEventListener('mouseup', handleMouseUp)
+
+        return ()=>{
+            window.removeEventListener('mouseup', handleMouseUp)
+            window.removeEventListener('mousemove', handleMouseMove)
+        }
+
+    },[resize])
 
     return(
-        <div className="scrollbar">
-            
-
+        <div  ref={scrollbarRef} className="scrollbar">
             <div className="fake-border">
-
-                <div className="border"></div>
+                <div onMouseDown={handleMouseDown} className="border"style={{right:position}} ></div>
             </div>
         </div>
     )
