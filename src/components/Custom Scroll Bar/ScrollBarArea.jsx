350f1d65599160e1467dbfe9c50ecf4cd0818e1e
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-03 18:12:48 +0000

    faking content that needs a scrollbar

---
+++ src/components/Custom Scroll Bar/ScrollBarArea.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/ScrollBarArea.css'
+import ScrollBar from './ScrollBar'
+function ScrollBarArea() {
+  return (
+    <div className='container'>
+      <ScrollBar/>
+    </div>
+  )
+}
+
+export default ScrollBarArea
