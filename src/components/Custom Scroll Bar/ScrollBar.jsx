edd1f073afe3eb62d0d12adacb30aa3d8bfd2014
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-04 09:09:34 +0000

    border resize div from left and right now

--- src/components/Custom Scroll Bar/ScrollBar.jsx
+++ src/components/Custom Scroll Bar/ScrollBar.jsx
@@ -3,32 +3,51 @@ import { useState, useEffect, useRef } from "react";
 
 const ScrollBar=()=>{
 
-    const MIN_WIDTH = 200;
+    const MIN_WIDTH_PERCENTAGE = 10/100;
     
     const scrollbarRef = useRef(null);
     const borderRight = useRef(null);
+    const borderLeft  = useRef(null);
     const scroller = useRef(null)
-    const [position, setPosition] = useState(0);
+    const [positionRight, setPositionRight] = useState(0);
+    const [positionLeft, setPositionLeft] = useState(0);
     const [resize, setResize] = useState(false);
+    const currentBorder= useRef('');
 
-    const handleMouseDown = (click)=>{
-        const element = click.target
-        const computedStyle = window.getComputedStyle(element)
+    const handleMouseDown = (clicked)=>{
         setResize(true)
+        currentBorder.current= (clicked.currentTarget.dataset.name)
     }
 
     const handleMouseUp = ()=>{
         setResize(false)
+        currentBorder.current = ""
+        scroller.current.style.cursor ="grab"
+    }
+
+    const grabAndScroll = ()=>{
+        scroller.current.style.cursor ="grabbing"
     }
 
     const handleMouseMove = (mousePosition)=>{
         const rect = scrollbarRef.current.getBoundingClientRect();
-        const borderPosition = mousePosition.clientX - rect.right;
-        borderRight.current.style.cursor ="ew-resize"
-        if (borderPosition <=0 && borderPosition >=-rect.width+MIN_WIDTH) setPosition(-borderPosition)
-         console.log("x relative to scrollbar:", borderPosition);
-    }
-
+        const scrollerTab = scroller.current.getBoundingClientRect();
+        const borderRightPosition = mousePosition.clientX - rect.right;
+        const borderLefttPosition = mousePosition.clientX - rect.left;
+        
+            if (currentBorder.current=='borderRight'){
+                borderRight.current.style.cursor ="ew-resize"
+                if ( scrollerTab.width >=rect.width*MIN_WIDTH_PERCENTAGE) setPositionRight(-borderRightPosition)
+                console.log("Width of Scrolling Tab:", scrollerTab.width);
+            
+            }
+            if (currentBorder.current=='borderLeft'){
+                borderLeft.current.style.cursor ="ew-resize"
+                if ( scrollerTab.width >=rect.width*MIN_WIDTH_PERCENTAGE) setPositionLeft(borderLefttPosition)
+                console.log("Width of Scrolling Tab:", scrollerTab.width);
+            }
+        }
+      
     useEffect(()=>{
         if (!resize) return;
         window.addEventListener('mousemove', handleMouseMove)
@@ -41,11 +60,18 @@ const ScrollBar=()=>{
 
     },[resize])
 
+
+    useEffect(()=>{
+
+    })
+
     return(
         <div  ref={scrollbarRef} className="scrollbar">
             <div className="fake-border">
-                <div   ref={borderRight} onMouseDown={handleMouseDown} className="border"style={{width:position+2}} ></div>
-                <div   ref={scroller} className="scroller"></div>
+                <div   data-name='borderRight' ref={borderRight} onMouseDown={handleMouseDown} className="border"style={{width:positionRight+2}} ></div>
+                <div   ref={scroller} onMouseUp={handleMouseUp} onMouseDown={grabAndScroll} className="scroller"></div>
+                <div   data-name='borderLeft' ref={borderLeft} onMouseDown={handleMouseDown} className="border"style={{width:positionLeft+2}} ></div>
+
             </div>
         </div>
     )
