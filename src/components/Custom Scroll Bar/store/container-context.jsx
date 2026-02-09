54e3b40f61f4d09c77a16350f7898c800921e236
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-02-09 15:59:43 +0000

    Created a context file

---
+++ src/components/Custom Scroll Bar/store/container-context.jsx
@@ -0,0 +1,30 @@
+import { useSearchParams } from 'next/navigation'
+import {createContext, useState} from 'react'
+
+const ContainerContext = createContext({
+
+    contentWidth:0
+
+})
+
+
+function ContainerContextProvider(props){
+
+    const [ContentWidth, setContentWidth] = useState([])
+
+    const updateContentWidth =(currentWidth)=>{
+
+        setContentWidth(currentWidth)
+
+    }
+
+    const synchroniser ={
+        contentWidth:ContentWidth
+    }
+
+    return <ContainerContext.Provider value={synchroniser}>
+
+        {props.children}
+
+    </ContainerContext.Provider>
+}
