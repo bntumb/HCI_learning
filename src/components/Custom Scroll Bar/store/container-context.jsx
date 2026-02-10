07623178ee81c2c0b1b9b5908be8e602f864438d
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-02-10 12:10:01 +0000

    Added update content position

--- src/components/Custom Scroll Bar/store/container-context.jsx
+++ src/components/Custom Scroll Bar/store/container-context.jsx
@@ -3,15 +3,16 @@ import {createContext, useState} from 'react'
 const ContainerContext = createContext({
 
     contentWidth:0,
-    updateWidth: (currentWidth)=>{}
-
-
+    contentPosition:0,
+    updateWidth: (currentWidth)=>{},
+    updatePosition: (currentPosition)=>{}
 })
 
 
 export function ContainerContextProvider(props){
 
     const [ContentWidth, setContentWidth] = useState([])
+    const [ContentPosition, setContentPosition] = useState([])
 
     const updateContentWidth =(currentWidth)=>{
 
@@ -19,9 +20,18 @@ export function ContainerContextProvider(props){
         console.log(currentWidth)
     }
 
+    
+    const updateContentPosition =(currentPosition)=>{
+
+        setContentPosition(currentPosition)
+        console.log(currentPosition)
+    }
+
     const synchroniser ={
-        contentWidth:ContentWidth,
-        updateWidth: updateContentWidth
+        contentWidth: ContentWidth,
+        contentPosition: ContentPosition,
+        updateWidth: updateContentWidth,
+        updatePosition: updateContentPosition
     }
 
     return <ContainerContext.Provider value={synchroniser}>
